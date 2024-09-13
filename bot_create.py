092b2187c2124713c838118ffcdfcab5ac93b4af
Aidar Iskhakov <aid.torres09@yandex.ru>
2024-09-13 08:38:31 +0300

    Fix small errors

--- bot_create.py
+++ bot_create.py
@@ -2,8 +2,12 @@ from os import environ
 
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import Dispatcher, Bot
+from dotenv import load_dotenv
+
+load_dotenv()
 
 token = environ.get('BOT_TOKEN')
 group_id = environ.get('GROUP_ID')
+
 bot = Bot(token=token)
 dp = Dispatcher(storage=MemoryStorage())
